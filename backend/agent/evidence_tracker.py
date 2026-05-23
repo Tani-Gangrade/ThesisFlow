4e395ad46f722b0387538c1aaa084612f12aa1b6
Tani-Gangrade <tanishagangrade29@gmail.com>
2026-05-23 17:47:03 +0530

    Removing unnnecessary files

--- backend/agent/evidence_tracker.py
+++
@@ -1,4 +0,0 @@
-def update_evidence(thesis_state, paper):
-    for section in paper["mapped_sections"]:
-        for claim in thesis_state["outline"]["Chapter 3"][section]["claims"]:
-            claim["current_evidence"] += 1
